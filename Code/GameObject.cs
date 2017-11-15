2d3e63876880593479337ba1d4b4d8e6223814ab
Blezzar <berggrenj@hotmail.com>
2017-11-15 11:06:58 +0100

    Add GameObject
    Added GameObject.cs
    Added variables: pos, tex, boundingBox
    Added method: Draw(SpriteBatch sb)

---
+++ Code/GameObject.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefenceINF
+{
+    abstract class GameObject
+    {
+
+        protected Vector2 pos;
+        protected Texture2D tex;
+        protected Rectangle boundingBox;
+
+        public GameObject(Texture2D tex, Vector2 pos)
+        {
+
+            this.pos = pos;
+            this.tex = tex;
+
+        }
+
+        public abstract void Draw(SpriteBatch sb);
+
+    }
+}
