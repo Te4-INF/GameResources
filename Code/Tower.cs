30bda91a8c3fcac5a4838bf165b40fae24d4b321
Blezzar <berggrenj@hotmail.com>
2017-11-15 11:20:23 +0100

    All Tower .cs files added
    Added Tower.cs, ArcherTower.cs, FireTower.cs, IceTower.cs, TowerHandler.cs

---
+++ Code/Tower.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefenceINF
+{
+    abstract class Tower: GameObject
+    {
+
+        public Tower(Texture2D tex, Vector2 pos): base(tex, pos)
+        {
+
+
+
+        }
+
+        public abstract override void Draw(SpriteBatch sb);
+    }
+}
