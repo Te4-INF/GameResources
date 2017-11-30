b50058a904a68587a0dfcda2174db49c5df0a586
MauritzLinderoth <98mauritz@gmail.com>
2017-11-30 12:18:37 +0100

    made backbufferhandler

--- GameResources/Code/BackBufferHandler.cs
+++ GameResources/Code/BackBufferHandler.cs
@@ -1,3 +1,5 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
@@ -12,38 +14,41 @@ namespace TowerDefenceINF
     {
         RenderTarget2D backgroundLayer;
 
-        List<Tower> towerList;
+        Texture2D mapTexture;
 
-        public BackBufferHandler(List<Tower> towerList, GraphicsDevice graphicsDevice)
+        public BackBufferHandler(List<Tower> towerList, GraphicsDevice graphicsDevice, ContentManager content)
         {
-
-            this.towerList = towerList;
+            mapTexture = content.Load<Texture2D>("meh");
             backgroundLayer = new RenderTarget2D(graphicsDevice, 1600, 900);
-
         }
 
-        private void DrawRenderTargetLayer(GraphicsDevice device)
-
+        private void Update(GraphicsDevice device, List<Tower> towerList)
         {
-
-            // skapa ny spriteBatch
-
             SpriteBatch sb = new SpriteBatch(device);
+            
+            // sätt till annat renderTarget
 
-            // sätt till annat renderTarget device.SetRenderTarget(backgroundLayer); device.Clear(Color.Transparent); sb.Begin();
+            device.SetRenderTarget(backgroundLayer);
+            device.Clear(Color.Transparent);
+            sb.Begin();
+
+
 
             foreach (Tower t in towerList)
-
             {
-
                 t.Draw(sb);
-
+                sb.Draw(mapTexture, new Vector2(0, 0), Color.White);
             }
+
             sb.End();
 
-            // Sätt tillbaka graphicsDevice till ordinarie fönster device.SetRenderTarget(null);
-
+            // Sätt tillbaka graphicsDevice till ordinarie fönster 
+            device.SetRenderTarget(null);
         }
 
+        public RenderTarget2D GetBackgroundLayer()
+        {
+            return backgroundLayer;
+        }
     }
 }
