9effa53246198d4fba1e7aff23fd29103f54a242
Blezzar <berggrenj@hotmail.com>
2017-12-01 12:30:04 +0100

    afehg

--- GameResources/Code/EnemyHandler.cs
+++ GameResources/Code/EnemyHandler.cs
@@ -3,7 +3,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace TowerDefenceINF
+namespace TowerDefenceINF.GameResources.Code
 {
     class EnemyHandler
     {
